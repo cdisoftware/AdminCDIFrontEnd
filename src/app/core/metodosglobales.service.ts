102a471a30d21a89773ba999fb2fdefa92181fe9
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2022-02-03 10:55:18 -0500

    Ajustes

--- src/app/core/metodosglobales.service.ts
+++ src/app/core/metodosglobales.service.ts
@@ -69,5 +69,7 @@ export class MetodosGlobalesService {
   insertatipobackup(bandera: string, BodyPost: any) {
     return this.http.post<any>(this.url_servidor + 'insertatipobackup/' + bandera, BodyPost);
   }
-
+  consultahardware(IdServidor: string, DiscoDuro: string, Ram: string, Procesador: string) {
+    return this.http.get<any[]>(this.url_servidor + 'consultahardware/' + IdServidor + '/' + DiscoDuro + '/' + Ram + '/' + Procesador);
+  }
 }
