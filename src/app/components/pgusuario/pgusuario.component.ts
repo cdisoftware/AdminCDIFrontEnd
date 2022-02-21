ec428d6b3bd99726eb3922ee17d2928a693d8b6c
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2022-02-21 16:00:02 -0500

    Ajustes actualizar información usuario

--- src/app/components/pgusuario/pgusuario.component.ts
+++ src/app/components/pgusuario/pgusuario.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { MetodosGlobalesService } from 'src/app/core/metodosglobales.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-pgusuario',
@@ -8,21 +10,45 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class PgusuarioComponent implements OnInit {
 
-  constructor(private cookies: CookieService) { }
+  constructor(private cookies: CookieService,
+    private Servicios: MetodosGlobalesService,
+    private _modalService: BsModalService) { }
 
   //Variables obtienen info
-  Nombre: string =  this.cookies.get('Nombre');
-  Apellido: string =  this.cookies.get('Apellido');
-  Usuario: string =  this.cookies.get('Usuario');
-  Password: string =  this.cookies.get('Password');
-  UjusteGuarda: boolean = false;
+  Nombre: string = this.cookies.get('Nombre');
+  Apellido: string = this.cookies.get('Apellido');
+
+  NombreEdit: string = this.Nombre;
+  ApellidoEdit: string = this.Apellido;
+  Usuario: string = this.cookies.get('Usuario');
+  Password: string = this.cookies.get('Password');
+  UjusteGuarda: boolean = true;
+
+  //variables modal mensajes
+  lblModalMsaje: string;
+  modalMensaje: BsModalRef;
   ngOnInit(): void {
   }
 
-  True(){
+  True() {
     this.UjusteGuarda = true;
   }
-  False(){
+  False(templateMensaje: TemplateRef<any>) {
     this.UjusteGuarda = false;
+    this.ActualizarInfo(templateMensaje);
+  }
+
+  ActualizarInfo(templateMensaje: TemplateRef<any>) {
+    const Actualizar = {
+      Id_U: 1011,
+      Nombre: this.NombreEdit,
+      Apellido: this.ApellidoEdit,
+      Usuario: this.Usuario,
+      Password: this.Password
+    }
+    this.Servicios.actualizainfousuario('2',Actualizar).subscribe(respu => {
+      this.modalMensaje = this._modalService.show(templateMensaje);
+      this.lblModalMsaje = respu;
+    });
   }
 }
