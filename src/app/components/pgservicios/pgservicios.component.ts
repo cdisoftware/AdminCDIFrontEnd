5630470829b6102566317fca4cdfa1c1681c69f8
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2022-03-04 13:24:15 -0500

    Ajustes

--- src/app/components/pgservicios/pgservicios.component.ts
+++ src/app/components/pgservicios/pgservicios.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MetodosGlobalesService } from 'src/app/core/metodosglobales.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: 'app-pgservicios',
@@ -9,6 +10,8 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
   styleUrls: ['./pgservicios.component.css']
 })
 export class PgserviciosComponent implements OnInit {
+  //Usuario
+  IdUsuarioCookies: string = this.cookies.get('IdUsuario');
   //Variables modal mensaje
   modalMensaje: BsModalRef;
   //Variables inicio de paguina
@@ -16,17 +19,35 @@ export class PgserviciosComponent implements OnInit {
   //Variables consulta proyecto
   ArrayGrilla: any;
   IdProyecto: string;
+
   //Variables iniciacion agregar
-  IdInicioListas: string = '0';
+  IdPrioridad: string = '0';
+  IdIntegrador: string = '0';
+  SP: string = '';
+  Exce: string = '';
+  IdTipoServicio: string = '0';
+  Observacion: string = '';
+  Observaciones: string = '';
 
   //Variables filtros
   Tiposervidor: string;
   Prioridad: string;
   Sp: string;
+
+  //Variables Fecha
+  Dia = new Date().getDate();
+  Mes = new Date().getMonth() + 1;
+  Año = new Date().getFullYear();
+
+  Fecha: string = this.Año + '-' + this.Mes + '-' + this.Dia;
+
+  //ListaUsario
+  ArregloListaUsuario: any;
   constructor(
     private activatedRoute: ActivatedRoute,
     private Servicios: MetodosGlobalesService,
-    private _modalService: BsModalService
+    private _modalService: BsModalService,
+    private cookies: CookieService
   ) { }
 
   ngOnInit(): void {
@@ -35,6 +56,7 @@ export class PgserviciosComponent implements OnInit {
     this.IdProyecto += Id;
 
     this.Grilla(this.Tiposervidor, this.Prioridad, this.Sp);
+    this.ListaUsuario();
   }
 
   Grilla(TipoServicio: string, Prioridad: string, Sp: string) {
@@ -49,15 +71,50 @@ export class PgserviciosComponent implements OnInit {
     }
     this.ArrayGrilla = [];
     this.Servicios.consultservicios(this.IdProyecto, TipoServicio, Prioridad, Sp).subscribe(respu => {
-      console.log(respu)
       if (respu.length > 0) {
         this.NombreProyecto = respu[0].NombreProyecto
         this.ArrayGrilla = respu;
       }
     })
   }
+  //Lista usuarios
+  ListaUsuario() {
+    const ConsultaUsu =
+    {
+      Nombre: "0",
+      Apellido: "0",
+      Cedula: "0"
+    }
+    this.ArregloListaUsuario = [];
+    this.Servicios.consultausuarios(ConsultaUsu).subscribe(respu => {
+      this.ArregloListaUsuario = respu;
+    })
+  }
 
-
+  AgregaServicio() {
+    if (this.IdIntegrador == '0' || this.IdIntegrador == undefined || this.SP == '' || this.SP == undefined || this.Exce == '' || this.Exce == undefined ||
+      this.IdTipoServicio == '0' || this.IdTipoServicio == undefined || this.Observacion == '' || this.Observacion == undefined || this.IdPrioridad == '' ||
+      this.IdPrioridad == undefined) {
+      console.log('Agrege todos los campos')
+    } else {
+      const InsertaServ = {
+        IdServicios: 0,
+        IdProyecto: this.IdProyecto,
+        FechaAsignacion: this.Fecha,
+        IdIntegrador: this.IdIntegrador,
+        StoredProcedures: this.SP,
+        EXEC_SP: this.Exce,
+        TipoServicio: this.IdTipoServicio,
+        Estado: 0,
+        IdUsuarioAsigna: this.IdUsuarioCookies,
+        Observacion: this.Observacion,
+        Prioridad: this.IdPrioridad
+      }
+      this.Servicios.insertaservicio('3', InsertaServ).subscribe(respu => {
+        this.Grilla(this.Tiposervidor, this.Prioridad, this.Sp);
+      })
+    }
+  }
 
 
 
