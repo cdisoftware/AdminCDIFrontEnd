31f7f7c5cd6802014bae6f1e5072357133d282d8
williambernal1003 <84921661+williambernal1003@users.noreply.github.com>
2022-03-22 11:36:24 -0500

    Ajustes

---
+++ src/app/components/pgetb/pgetb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pgetb',
+  templateUrl: './pgetb.component.html',
+  styleUrls: ['./pgetb.component.css']
+})
+export class PgetbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
