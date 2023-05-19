2d8096a2100171cb7e04d9189af1e7eb366919bc
William Bernal <84921661+williambernal1003@users.noreply.github.com>
2023-05-19 11:32:10 -0500

    Ajustes

--- src/app/core/conexionprincipal.service.ts
+++ src/app/core/conexionprincipal.service.ts
@@ -11,13 +11,15 @@ export class ConexionprincipalService {
     //ambiente de trabajo 1 desarrollo 2 produccion
     ambientedetrabajo: string = '2';
 
-    //    public url_ProduccionCDI = 'http://192.168.3.186:1003/AdministradorCDI/';
-    public url_ProduccionCDI = 'http://192.168.3.186:1005/AdministradorCDI/';
+    public url_ProduccionCDI = 'http://192.168.3.186:1003/AdministradorCDI/';
+    public url_DesarolloCDI = 'http://192.168.3.186:1005/AdministradorCDI/';
 
 
 
     SeleccionAmbiente() {
         if (this.ambientedetrabajo == '2') {
+            return this.url_DesarolloCDI;
+        }else if(this.ambientedetrabajo == '1'){
             return this.url_ProduccionCDI;
         }
         else {
