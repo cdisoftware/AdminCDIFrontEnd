e4545878efe0e3f9639dd3407377489f6daae295
Juan José Gutiérrez Leguizamón <68406983+JuanGut06@users.noreply.github.com>
2023-05-23 15:33:27 -0500

    ajustes publicaciones

--- src/app/core/conexionprincipal.service.ts
+++ src/app/core/conexionprincipal.service.ts
@@ -9,7 +9,7 @@ export class ConexionprincipalService {
 
     constructor(private http: HttpClient) { }
     //ambiente de trabajo 1 desarrollo 2 produccion
-    ambientedetrabajo: string = '1';
+    ambientedetrabajo: string = '2';
 
     public url_ProduccionCDI = 'http://192.168.3.186:1003/AdministradorCDI/';
     public url_DesarolloCDI = 'http://192.168.3.186:1005/AdministradorCDI/';
